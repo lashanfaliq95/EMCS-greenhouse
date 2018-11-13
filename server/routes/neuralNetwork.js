8658dbc2d35d714c0e36f3615ba51ffaf3ec16a0
himasha <himashasds@gmail.com>
2018-11-13 15:58:53 +0530

    call prediction

--- server/routes/neuralNetwork.js
+++ server/routes/neuralNetwork.js
@@ -2,7 +2,8 @@ var express = require('express');
 var fs = require('fs');
 var router = express.Router();
 
-var myPythonScript = "../NN/estimator_multilayer_perceptron.py";
+var trainingScript = "../NN/estimator_multilayer_perceptron.py";
+var predictScript = "../NN/restore.py";
 var pythonExecutable = "python";
 
 
@@ -14,7 +15,7 @@ var uint8arrayToString = function(data){
 /* GET users listing. */
 router.get('/train', function(req, res, next) {
     const spawn = require('child_process').spawn;
-    const scriptExecution = spawn(pythonExecutable, [myPythonScript]);
+    const scriptExecution = spawn(pythonExecutable, [trainingScript]);
     scriptExecution.stdout.on('data', (data) => {
         console.log(uint8arrayToString(data));
         res.send(uint8arrayToString(data));
@@ -36,4 +37,19 @@ router.get("/getData", function(req, res, next) {
     let accuracyJsonFile = JSON.parse(rawdata);
     res.json(accuracyJsonFile);
 });
+
+router.get("/predict", function (req, res, next) {
+    console.log("predicting");
+    let arg1 = '2';
+    let arg2 = '22';
+    let arg3 = '2';
+    let arg4 = '22';
+    const spawn = require('child_process').spawn;
+    const scriptExecution = spawn(pythonExecutable, [predictScript, arg1, arg2, arg3, arg4]);
+    scriptExecution.stdout.on('data', (data) => {
+        console.log(uint8arrayToString(data));
+        res.send(uint8arrayToString(data));
+    });
+});
+
 module.exports = router;
